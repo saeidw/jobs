317dce3ccd6c40f4d971303bfea4cf5e7d20ab43
Samer N <samer@nakfour.com>
2011-06-01 20:22:36 +0300

    Added title column to Job (make sure to rake:db migrate) - DRY'ed up the forms as per Zef's advice (thanks\!)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110531171636) do
+ActiveRecord::Schema.define(:version => 20110601164409) do
 
   create_table "jobs", :force => true do |t|
     t.datetime "created_at"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20110531171636) do
     t.string   "location"
     t.text     "description"
     t.integer  "owner_id"
+    t.string   "title"
   end
 
   create_table "owners", :force => true do |t|
